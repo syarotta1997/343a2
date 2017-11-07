e7669139de3ed730319e2c0e90c35952560ae857
syarotta1997 <helenlu1997@gmail.com>
2017-11-07 16:10:54 -0500

    1

--- q1.sql
+++ q1.sql
@@ -27,7 +27,7 @@ DROP VIEW IF EXISTS above_fourty CASCADE;
 
 -- selects all year from 1996 to 2016 inclusive, with elections held during the years and paritipant countries
 CREATE VIEW elections_results AS
-    select extract( year from e_date) as year, election.id, country_id, party_id, avg(votes / votes_valid) as percent, 
+    select extract( year from e_date) as year, election.id, country_id, party_id, avg(votes / votes_valid) as percent 
     from election join election_result on election.id == election_result.election_id
     where extract (year from e_date) >= 1996 and extract (year from e_date) <= 2016
                and country_id <> NULL and party_id <> NULL and votes_valid <> NULL and votes <> NULL
