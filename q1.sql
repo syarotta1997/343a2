bd03e1c1732d23d5f436c08d14a29a2bc673a002
syarotta1997 <helenlu1997@gmail.com>
2017-11-07 16:36:15 -0500

    1

--- q1.sql
+++ q1.sql
@@ -33,48 +33,47 @@ CREATE VIEW elections_results AS
                and country_id <> NULL and party_id <> NULL and votes_valid <> NULL and votes <> NULL
     group by extract( year from e_date), country_id, party_id;
 
-create view below_five as
-    select year, country.name as countryName, '(0-5]' as voteRange, party_id
-    from elections_results join country on elections_results.country_id = country.id
-    where percent > 0.0 and percent <= 5.0;
-
-create view five_to_ten as
-    select year, country.name as countryName, '(5-10]' as voteRange, party_id
-    from elections_results join country on elections_results.country_id = country.id
-    where percent > 5.0 and percent <= 10.0;
-
-create view ten_to_twenty as
-    select year, country.name as countryName, '(10-20]' as voteRange, party_id
-    from elections_results join country on elections_results.country_id = country.id
-    where percent > 10.0 and percent <= 20.0;
-
-create view twenty_thirty as
-    select year, country.name as countryName, '(20-30]' as voteRange, party_id
-    from elections_results join country on elections_results.country_id = country.id
-    where percent > 20.0 and percent <= 30.0;
-
-create view thirty_fourty as
-    select year, country.name as countryName, '(30-40]' as voteRange, party_id
-    from elections_results join country on elections_results.country_id = country.id
-    where percent >30.0 and percent <= 40.0;
-
-create view above_fourty as
-    select year, country.name as countryName, '(40-100]' as voteRange, party_id
-    from elections_results join country on elections_results.country_id = country.id
-    where percent > 40.0 and percent <= 100.0;
-
 create view all_party_votes as
+    (select year, country.name as countryName, '(0-5]' as voteRange, party_id
+        from elections_results join country on elections_results.country_id = country.id
+        where percent > 0.0 and percent <= 5.0)
+
+    union
+
+    (select year, country.name as countryName, '(5-10]' as voteRange, party_id
+        from elections_results join country on elections_results.country_id = country.id
+        where percent > 5.0 and percent <= 10.0)
+
+    union
+
+    (select year, country.name as countryName, '(10-20]' as voteRange, party_id
+        from elections_results join country on elections_results.country_id = country.id
+        where percent > 10.0 and percent <= 20.0)
+
+    union
+
+    (select year, country.name as countryName, '(20-30]' as voteRange, party_id
+            from elections_results join country on elections_results.country_id = country.id
+            where percent > 20.0 and percent <= 30.0)
+
+    union
+
+    (select year, country.name as countryName, '(30-40]' as voteRange, party_id
+            from elections_results join country on elections_results.country_id = country.id
+            where percent >30.0 and percent <= 40.0)
+
+    union
+
+    (select year, country.name as countryName, '(40-100]' as voteRange, party_id
+            from elections_results join country on elections_results.country_id = country.id
+            where percent > 40.0 and percent <= 100.0);
+
+create view answer as
     select year, countryName, voteRange, party.name
-    from (select * from (select * from below_five as v1 ) 
-                                    union (select * from five_to_ten as v2 ) 
-                                    union (select * from ten_to_twenty as v3) 
-                                    union (select * from twenty_thirty as v4) 
-                                    union (select * from thirty_fourty  as v5)
-                                    union (select * from above_fourty as v6) as all_votes)  join party on all_votes.party_id = party.id;
+    from all_party_votes  join party on all_votes.party_id = party.id;
 
 -- the answer to the query 
 insert into q1
-    (select *
-    from all_party_votes as answer);
+    (select * from answer);
     
 
