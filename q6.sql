fcd8607e7d60d4bf700df2497083c65335f1da70
syarotta1997 <helenlu1997@gmail.com>
2017-11-08 15:13:12 -0500

    1

--- q6.sql
+++ q6.sql
@@ -17,14 +17,14 @@ CREATE TABLE q6(
 -- that define your intermediate steps.  (But give them better names!)
 DROP VIEW IF EXISTS cabinets_null  CASCADE;
 DROP VIEW IF EXISTS cabinets_notnull  CASCADE;
-DROP VIEW IF EXISTS cabinets_answer  CASCADE;
+DROP VIEW IF EXISTS answer  CASCADE;
 
 -- Define views for your intermediate steps here.
 create view cabinets_null as
 select (select min(start_date) from cabinet as c2 where c1.start_date < c2.start_date 
                                                                              and c1.country_id = c2.country_id) as endDate,
           c1.start_date as startDate,
-          c1.id as cabinetId
+          c1.id as cabinetId,
           null as pmParty;
 from cabinet as c1 join country on c1.country_id = country.id
                              join cabinet_party on c1.id = cabinet_party.cabinet.id
@@ -34,7 +34,7 @@ create view cabinets_notnull as
 select (select min(start_date) from cabinet as c2 where c1.start_date < c2.start_date 
                                                                              and c1.country_id = c2.country_id ) as endDate,
           c1.start_date as startDate,
-          c1.id as cabinetId
+          c1.id as cabinetId,
           party.name as pmParty;
 from cabinet as c1 join country on c1.country_id = country.id
                              join cabinet_party as cp on c1.id = cp.cabinet.id
