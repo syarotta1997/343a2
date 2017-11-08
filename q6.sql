0883332df41f99e2fef5953739397a78c7bcb71f
syarotta1997 <helenlu1997@gmail.com>
2017-11-08 15:37:26 -0500

    1

--- q6.sql
+++ q6.sql
@@ -24,7 +24,7 @@ create view cabinets_null as
 select (select min(c2.start_date) from cabinet as c2 where c1.start_date < c2.start_date 
                                                                              and c1.country_id = c2.country_id) as endDate,
           c1.start_date as startDate,
-          c1.id as cabinetId,
+          distinct c1.id as cabinetId,
           null as pmParty,
           country.name as countryName
 from cabinet as c1 join country on c1.country_id = country.id
@@ -38,7 +38,7 @@ create view cabinets_notnull as
 select (select min(c2.start_date) from cabinet as c2 where c1.start_date < c2.start_date 
                                                                              and c1.country_id = c2.country_id ) as endDate,
           c1.start_date as startDate,
-          c1.id as cabinetId,
+          distinct c1.id as cabinetId,
           party.name as pmParty,
           country.name as countryName
 from cabinet as c1 join country on c1.country_id = country.id
