d1ca06a57e32498a8c980ca9ef89aa803a8cb033
syarotta1997 <helenlu1997@gmail.com>
2017-11-08 13:25:27 -0500

    1

--- q3.sql
+++ q3.sql
@@ -14,7 +14,6 @@ create table q3(
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
 DROP VIEW IF EXISTS participation_ratio CASCADE;
-DROP VIEW IF EXISTS avg_ratio CASCADE;
 DROP VIEW IF EXISTS not_increasing CASCADE;
 DROP VIEW IF EXISTS increasing CASCADE;
 DROP VIEW IF EXISTS answer CASCADE;
@@ -23,36 +22,32 @@ DROP VIEW IF EXISTS answer CASCADE;
 
 --selects country with at least one election and calculates its participation ratio in year 2001 - 2016 inclusive
 CREATE VIEW participation_ratio AS
-    select extract(year from e_date) as year, country_id as cid, election.id as eid,
-              cast ( (votes_cast+0.0) / electorate as numeric) as ratio
+    select extract(year from e_date) as year, country_id as cid, 
+              avg( cast ( (votes_cast+0.0) / electorate as numeric)) as ratio
     from election
     where extract(year from e_date) >= '2001' and extract(year from e_date) <= '2016'
                 -- this line below will eliminate any country who do not hold election during above years
                 -- need to enforce not-null on votes cast since it could be null, however electorate is already
                 -- restrainted to be not null thus we can use without doubt. 
-               and country_id is not NULL and votes_cast is not NULL;
+               and country_id is not NULL and votes_cast is not NULL
+    group by extract( year from e_date), country_id;
 
-create view avg_ratio as
-select year, cid, avg(ratio) as ratio
-from participation_ratio
-group by year,cid;
-
-select * from avg_ratio order by year desc;
+select * from participation_ratio order by year desc;
 
 -- choose countries
 create view not_increasing as
     select p1.cid, p1.year, p1.ratio
-    from avg_ratio as p1
+    from participation_ratio as p1
     where p1.ratio > any( select p2.ratio 
-                                      from avg_ratio as p2 
+                                      from participation_ratio as p2 
                                       where p1.year < p2. year and p1.cid = p2.cid);
 
 create view increasing as
- (select distinct cid from avg_ratio) except (select distinct cid from not_increasing);
+ (select distinct cid from participation_ratio) except (select distinct cid from not_increasing);
 
 create view answer as
 select increasing.cid, country.name as countryName, p.year as year, p.ratio as participationRatio
-from increasing join avg_ratio as p on increasing.cid = p.cid
+from increasing join participation_ratio as p on increasing.cid = p.cid
                          join country on increasing.cid = country.id;
 
 select * from answer order by countryName desc, year desc;
