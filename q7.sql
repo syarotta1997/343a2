343229e7cf74b6723fa00e62e8c4d9566fa662be
syarotta1997 <helenlu1997@gmail.com>
2017-11-08 16:48:35 -0500

    1

--- q7.sql
+++ q7.sql
@@ -15,6 +15,7 @@ CREATE TABLE q7(
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
 DROP VIEW IF EXISTS alliances_in_a_country CASCADE;
+DROP VIEW IF EXISTS alliances CASCADE;
 
 -- Define views for your intermediate steps here.
 
@@ -24,10 +25,17 @@ from election join election_result on election.id = election_result.election_id
 where country_id is not null and alliance_id is not null
 order by election.id, party_id;
 
-select * from alliances_in_a_country;
 
 
+create view alliances as
+select e1.party_id as pid1, e2.party_id as pid2
+from election_result as e1 join election_result as e2
+where e1.alliance_id is not null and
+          e1.alliance_id = e2.id and
+          e1. party_id < e2.party_id
+order by e1.party_id;
 
+select * from alliances;
 
 -- the answer to the query 
 --insert into q7 
