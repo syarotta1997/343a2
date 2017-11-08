d4638da832323adb44d9147ac5abe01f178bb647
syarotta1997 <helenlu1997@gmail.com>
2017-11-08 13:22:44 -0500

    1

--- q3.sql
+++ q3.sql
@@ -22,25 +22,29 @@ DROP VIEW IF EXISTS answer CASCADE;
 
 --selects country with at least one election and calculates its participation ratio in year 2001 - 2016 inclusive
 CREATE VIEW participation_ratio AS
-    select extract(year from e_date) as year, country_id as cid, 
-              avg( cast ( (votes_cast+0.0) / electorate as numeric)) as ratio
+    select extract(year from e_date) as year, country_id as cid, election.id as eid,
+              cast ( (votes_cast+0.0) / electorate as numeric) as ratio
     from election
     where extract(year from e_date) >= '2001' and extract(year from e_date) <= '2016'
                 -- this line below will eliminate any country who do not hold election during above years
                 -- need to enforce not-null on votes cast since it could be null, however electorate is already
                 -- restrainted to be not null thus we can use without doubt. 
                and country_id is not NULL and votes_cast is not NULL
-    group by extract( year from e_date), country_id;
 
-select * from participation_ratio order by year desc;
+create view avg_ratio as
+select year, cid, avg(ratio) as ratio
+from participation_ratio
+group by year,cid;
+
+select * from avg_ratio order by year desc;
 
 -- choose countries
 create view not_increasing as
-select p1.cid, p1.year, p1.ratio
-from participation_ratio as p1
-where p1.ratio > any( select p2.ratio 
-                                  from participation_ratio as p2 
-                                  where p1.year < p2. year and p1.cid = p2.cid);
+    select p1.cid, p1.year, p1.ratio
+    from participation_ratio as p1
+    where p1.ratio > any( select p2.ratio 
+                                      from participation_ratio as p2 
+                                      where p1.year < p2. year and p1.cid = p2.cid);
 
 create view increasing as
  (select distinct cid from participation_ratio) except (select distinct cid from not_increasing);
