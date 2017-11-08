a39962c6a91074253f5482624cf602a87d2898e2
syarotta1997 <helenlu1997@gmail.com>
2017-11-08 01:19:31 -0500

    1

--- q3.sql
+++ q3.sql
@@ -17,6 +17,15 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
 
+CREATE VIEW participation_ratio AS
+    select extract(year from e_date) as year, country_id, avg( (votes_cast+0.0) /electrorate) as ratio
+    from election join election_result on election.id = election_result.election_id
+    where extract(year from e_date) >= '2001' and extract(year from e_date) <= '2016'
+               and country_id is not NULL and votes_cast is not NULL
+    group by extract( year from e_date), country_id;
+
+select * from participation_ratio;
+
 
 -- the answer to the query 
 insert into q3 
---
+++ q3_order.sql
@@ -0,0 +1,2 @@
+select * from q2 order by countryName asc, wonElections asc, partyName desc;
+
