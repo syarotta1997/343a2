5bb63b6a503fe972aa32d0bed57408eaf5bf9b55
syarotta1997 <helenlu1997@gmail.com>
2017-11-08 17:01:57 -0500

    1

--- q7.sql
+++ q7.sql
@@ -19,19 +19,15 @@ DROP VIEW IF EXISTS alliances CASCADE;
 
 -- Define views for your intermediate steps here.
 create view alliances as
-select election.country_id as cid, e1.election_id as eid, e1.party_id as pid1, e2.party_id as pid2
+select election.country_id as cid, e1.party_id as pid1, e2.party_id as pid2, count(election.id) as counts
 from election_result as e1 join election_result as e2 on e1.alliance_id = e2.id
          join election on e1.election_id = election.id
 where e1.election_id = e2.election_id and e1. party_id < e2.party_id
+group by election.country_id, e1.party_id, e2.party_id
 order by e1.party_id;
 
 select * from alliances;
 
-create view alliances_in_a_country as
-select eid, pid1,pid2
-from alliances join election on alliances.eid = election.id
-where country_id is not null
-order by pid1;
 
 -- the answer to the query 
 --insert into q7 
