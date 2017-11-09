d61fce69277f089779947a433ccae837ccf6c7a7
syarotta1997 <helenlu1997@gmail.com>
2017-11-09 00:05:28 -0500

    1

--- q7.sql
+++ q7.sql
@@ -42,7 +42,7 @@ order by e1.party_id;
 select * from alliances_reci order by cid,pid1;
 
 create view total_ally_count as
-select a1.cid, a1.pid1, .a1.pid2, sum(a1.counts) as counts
+select a1.cid, a1.pid1, a1.pid2, sum(a1.counts) as counts
 from (select * from alliances union all select * from alliances_reci) as a1 
 group by a1.cid,a1.pid1,a1.pid2
 order by a1.pid1;
